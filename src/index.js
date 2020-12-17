371da95922a8ecf0a2d8ac3ea8d42ab4ec66d3ac
Arseni Pantsialei <arsenipantsialei@gmail.com>
2020-12-17 22:40:05 +0300

    feat: add map and indicators

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { mapInit } from './js/map.js';
+
+
+console.log('webpack!');
+
+//https://api.openweathermap.org/data/2.5/weather?q=city&lang=en&appid=dd286db0aa32ac081394420a041263de&units=metric
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  mapInit();
+});
