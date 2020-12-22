e5e6eafbffadde43a3f23e5fd6073422dff55901
Arseni Pantsialei <arsenipantsialei@gmail.com>
2020-12-23 00:18:06 +0300

    feat: rebase develop

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import { tableGlobalCases } from './js/views/tableGlobalCases.js';
 import GraphInit from './js/graphics/app';
+import { mapInit } from './js/map.js';
+
 
 tableGlobalCases.init();
 GraphInit();
+
+console.log('webpack!');
+
+//https://api.openweathermap.org/data/2.5/weather?q=city&lang=en&appid=dd286db0aa32ac081394420a041263de&units=metric
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  mapInit();
+});
